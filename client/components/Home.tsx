f1c96b22fdd7ccb1eefb9443b94449021d4278f6
Abe Calder <abecalder1@gmail.com>
2025-09-25 16:29:34 +1200

    completed signin page with functioning auth0 login / out

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   const handleSignIn = () => {
     loginWithRedirect({
       authorizationParams: {
-        redirectUri: `${window.location.origin}/login`,
+        redirectUri: `${window.location.origin}/dashboard`,
       },
     })
   }
@@ -24,11 +24,34 @@ export default function Home() {
           <h1>Welcome!</h1>
         </IfAuthenticated>
         <IfNotAuthenticated>
-          <div className='sign-in-wrapper'>
+          <img
+            alt="mini-CapitaVault-logo"
+            className="mini-sign-in-page-logo"
+            src="/images/CapitaVault-logo.webp"
+          ></img>
+          <div className="sign-in-page-logo-wrapper">
+            <img
+              alt="CapitaVault-logo"
+              className="sign-in-page-logo"
+              src="/images/CapitaVault-logo.webp"
+            ></img>
+            <h1 className="sign-in-page-heading">
+              CapitaVault is an <br></br> investment portfolio <br></br> made
+              easy
+            </h1>
+            <p className="sign-in-page-sub-heading">
+              All your Crypto, shares and other <br></br> holdings, held easily
+              in one place.
+            </p>
+          </div>
+          <div className="sign-in-wrapper">
             <button className="sign-in-button" onClick={handleSignIn}>
               Log In
             </button>
-            <h1 className="sign-in-message">Please</h1>
+            <h1 className="sign-in-message">
+              Please press log in to view or <br></br> create your investment
+              portfolio
+            </h1>
           </div>
         </IfNotAuthenticated>
       </div>
--- client/routes.tsx
+++ client/routes.tsx
@@ -2,10 +2,12 @@ import { createRoutesFromElements, Route } from 'react-router'
 
 import App from './components/App.tsx'
 import Home from './components/Home.tsx'
+import Dashboard from './components/Dashboard.tsx'
 
 
 export default createRoutesFromElements(
   <Route path="/" element={<App />}>
     <Route index element={<Home />} />
+    <Route path='/dashboard' element={<Dashboard />} />
   </Route>,
 )
