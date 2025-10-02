49fc89ebbc0f50c1831bd645d9676a8df958b46c
Abe Calder <abecalder1@gmail.com>
2025-10-02 22:45:06 +1300

    got asset shares * asset price working and displaying and a clause for if one or more of the tickers is undefined

--- client/hooks/usePolygon.ts
+++ client/hooks/usePolygon.ts
@@ -1,9 +1,7 @@
 import {
   useQuery,
-  useQueries
 } from '@tanstack/react-query'
 import getAssetDataByTicker from '../apis/polygon'
-import { Asset } from '../../models/assets'
 
 export function useGetAssetDataByTicker(ticker: string) {
   const query = useQuery({
@@ -19,15 +17,15 @@ export function useGetAssetDataByTicker(ticker: string) {
 
 
 
-export const useAssetsQueries = (userAssetData: Asset[]) => {
-  const queries =
-    userAssetData?.map((asset) => ({
-      queryKey: ['assetData', asset.ticker],
-      queryFn: () => getAssetDataByTicker(asset.ticker), 
-      enabled: !!userAssetData, 
-    })) ?? []
+// export const useAssetsQueries = (userAssetData: Asset[]) => {
+//   const queries =
+//     userAssetData?.map((asset) => ({
+//       queryKey: ['assetData', asset.ticker],
+//       queryFn: () => getAssetDataByTicker(asset.ticker), 
+//       enabled: !!userAssetData, 
+//     })) ?? []
 
-  const results = useQueries({ queries: queries })
+//   const results = useQueries({ queries: queries })
 
-  return { queries: results }
-}
+//   return { queries: results }
+// }
--- server/routes/polygon.ts
+++ server/routes/polygon.ts
@@ -9,16 +9,38 @@ yesterday.setDate(yesterday.getDate() - 1)
 const formattedDate = yesterday.toISOString().slice(0, 10)
 
 
-// GET CryptoCurrency 
-router.get('/:ticker', async (req, res) => {
-  const ticker = String(req.params.ticker)
-  const response = await request
-    .get(
-      `https://api.polygon.io/v2/aggs/ticker/X:${ticker}USD/range/1/day/${formattedDate}/${formattedDate}`,
+// // GET CryptoCurrency 
+// router.get('/:ticker', async (req, res) => {
+//   const ticker = String(req.params.ticker)
+//   const response = await request
+//     .get(
+//       `https://api.polygon.io/v2/aggs/ticker/X:${ticker}USD/range/1/day/${formattedDate}/${formattedDate}`,
+//     )
+//     .set('Authorization', `Bearer ${process.env.POLY_API_KEY}`)
+//   console.log(response.body)
+//   res.json(response.body)
+// })
+
+router.get('/', async (req, res) => {
+  const tickersArr: string[] | string | undefined = String(req.query.tickers)
+  if (tickersArr) {
+    let ticker: string[] = tickersArr.split(',')
+    ticker = ticker.filter((t) => t && t !== 'undefined')
+    if (ticker.length === 0) {
+      return res.status(400).json({ error: 'No valid tickers provided.' })
+    }
+    const promises = ticker.map((t) =>
+      request
+        .get(
+          `https://api.polygon.io/v2/aggs/ticker/${t}/range/1/day/${formattedDate}/${formattedDate}`,
+        )
+        .set('Authorization', `Bearer ${process.env.POLY_API_KEY}`),
     )
-    .set('Authorization', `Bearer ${process.env.POLY_API_KEY}`)
-  console.log(response.body)
-  res.json(response.body)
+    const responses = await Promise.all(promises)
+    const results = responses.map((response) => response.body)
+    console.log(results)
+    res.json(results)
+  }
 })
 
 
