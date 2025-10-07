4a5386e1c3bcfbbac6252ec358f2bc1a585f182f
Abe Calder <abecalder1@gmail.com>
2025-10-07 15:55:34 +1300

    profile-settings heading and profile image

--- client/components/ProfileSettings.tsx
+++ client/components/ProfileSettings.tsx
@@ -1,11 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import Settings from "./Settings";
 
 export default function ProfileSettings() {
+  const { user } = useAuth0()
   return (
     <>
       <Settings />
       <div className="profile-settings-wrapper">
         <h1 className="profile-settings-heading">Profile Settings</h1>
+        <img alt='user-profile-image' src={ user && user.picture}></img>
       </div>
     </>
   )
