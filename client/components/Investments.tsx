07aad073c4595b7171fa1ff661fcb1aefd1ef712
Abe Calder <abecalder1@gmail.com>
2025-10-09 13:13:12 +1300

    start on investments page

---
+++ client/components/Investments.tsx
@@ -0,0 +1,26 @@
+import Nav from "./Nav";
+
+export default function Investments() {
+
+  return (
+    <>
+      <div className="app2">
+        <Nav />
+        <div className="investments-wrapper">
+          <div className="investments-heading-profile-overview-wrapper">
+            <h1 className="investments-heading">Investments</h1>
+            <div className="total-investments-wrapper">
+
+            </div>
+            <div className="number-of-investments-wrapper">
+
+            </div>
+            <div className="rate-of-return-wrapper">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
