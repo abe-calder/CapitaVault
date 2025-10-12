e64744bd28b22c65e42abccb01b3e2777802f0aa
Abe Calder <abecalder1@gmail.com>
2025-10-12 19:12:44 +1300

    add total invested value

--- client/components/Investments.tsx
+++ client/components/Investments.tsx
@@ -1,7 +1,16 @@
 import Nav from './Nav'
 import TopRightProfile from './TopRightProfile'
+import { usePortfolio } from '../context/PortfolioContext'
 
 export default function Investments() {
+  const { totalBalance, totalCost, income, pieChartData, setConvertCurrency, convertCurrency } = usePortfolio()
+  
+  function handleToggleCurrency(e: React.MouseEvent<HTMLButtonElement>): void {
+    e.preventDefault()
+    const selectedCurrency = (e.target as HTMLButtonElement).value
+    setConvertCurrency(selectedCurrency)
+  }
+
   return (
     <>
       <div className="app2">
@@ -17,6 +26,7 @@ export default function Investments() {
                 src="/images/hand-and-dollar-sign-icon.webp"
               ></img>
               <h1 className="total-investments-heading">Total Invested</h1>
+              <h2 className="total-invested-value">${totalCost.toFixed(2)}</h2>
             </div>
             <div className="number-of-investments-wrapper">
               <img
@@ -36,6 +46,46 @@ export default function Investments() {
               ></img>
               <h1 className="rate-of-return-heading">Rate of Return</h1>
             </div>
+          </div>
+          <div className="investments-buttons">
+            <label htmlFor="toggle-currency" className="toggle-currency-label">
+              Toggle Currency
+              <button
+                className="toggle-currency-buttons"
+                value="USD"
+                onClick={(e) => handleToggleCurrency(e)}
+              >
+                USD
+              </button>
+              <button
+                className="toggle-currency-buttons"
+                value="NZD"
+                onClick={(e) => handleToggleCurrency(e)}
+              >
+                NZD
+              </button>
+              <button
+                className="toggle-currency-buttons"
+                value="AUD"
+                onClick={(e) => handleToggleCurrency(e)}
+              >
+                AUD
+              </button>
+              <button
+                className="toggle-currency-buttons"
+                value="EUR"
+                onClick={(e) => handleToggleCurrency(e)}
+              >
+                EUR
+              </button>
+              <button
+                className="toggle-currency-buttons"
+                value="GBP"
+                onClick={(e) => handleToggleCurrency(e)}
+              >
+                GBP
+              </button>
+            </label>
           </div>
         </div>
       </div>
